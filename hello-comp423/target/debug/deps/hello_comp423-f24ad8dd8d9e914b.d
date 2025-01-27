0ded51b8a093d28551f5b1e54d18badb6faf2e4f
Shanyu Gowdu <gowdu@unc.edu>
2025-01-27 03:27:51 +0000

    Completed Hello World

---
+++ hello-comp423/target/debug/deps/hello_comp423-f24ad8dd8d9e914b.d
@@ -0,0 +1,5 @@
+/workspaces/RustSetup/hello-comp423/target/debug/deps/libhello_comp423-f24ad8dd8d9e914b.rmeta: src/main.rs
+
+/workspaces/RustSetup/hello-comp423/target/debug/deps/hello_comp423-f24ad8dd8d9e914b.d: src/main.rs
+
+src/main.rs:
